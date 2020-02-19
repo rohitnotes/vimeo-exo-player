24cd6f9e2565a37a4b8e1d5618aa664b161cddf0
Vikas Kukreti <vickydvlpr@gmail.com>
2020-02-20 00:08:30 +0530

    Layout fixes and added multi quality support

--- app/src/main/java/com/vikas/videoplayer/MainActivity.java
+++ app/src/main/java/com/vikas/videoplayer/MainActivity.java
@@ -1,23 +1,26 @@
 package com.vikas.videoplayer;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.pm.ActivityInfo;
 import android.media.MediaFormat;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
+import android.view.DragEvent;
+import android.view.GestureDetector;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.Format;
@@ -25,6 +28,7 @@ import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
@@ -46,25 +50,36 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton playerSettings;
+    static boolean fullscreen = false;
+    ImageButton playerSettings, fullScreen;
+    FrameLayout playerContainer;
     ProgressBar progressBar;
-    List<String> mediaList;
+    List<VideoTrack> videoTracks;
+    List<Song> songs;
     SimpleExoPlayer simpleExoPlayer;
+    private PopupMenu popup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // store songs
+        songs = new ArrayList<>();
+        songs.add(new Song("Sooraj Duba Hai Yaron", "Arijit Singh", "115637298"));
+        songs.add(new Song("Duaa live", "Arijit Singh", "116975711"));
+        songs.add(new Song("Blank Space", "Someone Fool", "115076220"));
+        songs.add(new Song("Numb Encore", "Linkin Park", "391036452"));
+        songs.add(new Song("Arijit Royal Stag Performance", "Arijit Singh", "95594048"));
+
+        // main code here
+        playerContainer = findViewById(R.id.player_container);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         if (getSupportActionBar() != null)
             getSupportActionBar().hide();
-        try {
-            initPlayer();
-        } catch (Exception e) {
-            Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-        }
-        if(getWindow() != null)
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        initPlayer();
     }
 
     @Override
@@ -109,21 +124,80 @@ public class MainActivity extends AppCompatActivity {
 
     private void initPlayer() {
         try {
-            PlayerView videoPlayer = findViewById(R.id.player_view);
+            fullScreen = findViewById(R.id.fullscreen_btn);
+            fullScreen.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (fullscreen) {
+                        exitFullScreen();
+                    } else {
+                        setFullScreen();
+                    }
+                }
+            });
+            final PlayerView videoPlayer = findViewById(R.id.player_view);
+            videoPlayer.setOnDragListener(new View.OnDragListener() {
+                @Override
+                public boolean onDrag(View v, DragEvent event) {
+                    Toast.makeText(MainActivity.this, "Drag" + event.toString(), Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            });
+            final GestureDetector gestureDetector = new GestureDetector(MainActivity.this, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onDoubleTap(MotionEvent e) {
+                    if (videoPlayer.getResizeMode() == AspectRatioFrameLayout.RESIZE_MODE_ZOOM)
+                        videoPlayer.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FIT);
+                    else
+                        videoPlayer.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_ZOOM);
+                    return false;
+                }
+            });
+            videoPlayer.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    gestureDetector.onTouchEvent(event);
+                    return false;
+                }
+
+            });
+
             simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(this);
             videoPlayer.setPlayer(simpleExoPlayer);
             progressBar = findViewById(R.id.loader);
             playerSettings = findViewById(R.id.player_settings);
+            popup = new PopupMenu(this, playerSettings);
+            MenuInflater inflater = popup.getMenuInflater();
+            inflater.inflate(R.menu.video_settings, popup.getMenu());
             playerSettings.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     showMenu(v);
                 }
             });
+
+            popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.video_quality_high:
+                            setMediaQuality("HIGH");
+                            break;
+                        case R.id.video_quality_medium:
+                            setMediaQuality("NORMAL");
+                            break;
+                        case R.id.video_quality_low:
+                            setMediaQuality("LOW");
+                            break;
+                    }
+                    item.setChecked(true);
+                    return false;
+                }
+            });
             simpleExoPlayer.addListener(new Player.EventListener() {
                 @Override
                 public void onLoadingChanged(boolean isLoading) {
-                    if(isLoading) {
+                    if (isLoading) {
                         progressBar.setVisibility(View.VISIBLE);
                     } else {
                         progressBar.setVisibility(View.INVISIBLE);
@@ -132,10 +206,10 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-                    if(playbackState == SimpleExoPlayer.DISCONTINUITY_REASON_SEEK_ADJUSTMENT) {
+                    if (playbackState == SimpleExoPlayer.DISCONTINUITY_REASON_SEEK_ADJUSTMENT) {
                         progressBar.setVisibility(View.VISIBLE);
                     }
-                    if(playbackState == simpleExoPlayer.STATE_READY) {
+                    if (playbackState == simpleExoPlayer.STATE_READY) {
                         progressBar.setVisibility(View.INVISIBLE);
                     }
                 }
@@ -143,31 +217,19 @@ public class MainActivity extends AppCompatActivity {
             simpleExoPlayer.setVideoFrameMetadataListener(new VideoFrameMetadataListener() {
                 @Override
                 public void onVideoFrameAboutToBeRendered(
-                    long presentationTimeUs,
-                    long releaseTimeNs,
-                    Format format,
-                    @Nullable MediaFormat mediaFormat
+                        long presentationTimeUs,
+                        long releaseTimeNs,
+                        Format format,
+                        @Nullable MediaFormat mediaFormat
                 ) {
                     // comments section
                 }
 
             });
 
-            playVideo("263034305");
-
             simpleExoPlayer.setPlayWhenReady(true);
-            simpleExoPlayer.addListener(new Player.EventListener() {
 
-                @Override
-                public int hashCode() {
-                    return super.hashCode();
-                }
-
-                @Override
-                public void onLoadingChanged(boolean isLoading) {
-
-                }
-            });
+            playVideo(songs.get(0).getVimeoId());
 
         } catch (Exception e) {
             Log.d("error", "initPlayer: " + e.getLocalizedMessage());
@@ -176,12 +238,14 @@ public class MainActivity extends AppCompatActivity {
 
     private void setMediaQuality(String videoQuality) {
         Uri uri;
-        if(videoQuality.equals("LOW")) {
-            uri = Uri.parse(mediaList.get(0));
-        } else if(videoQuality.equals("HIGH")) {
-            uri = Uri.parse(mediaList.get(mediaList.size() - 2));
+        if (videoQuality.equals("HIGH") && videoTracks.size() - 1 > 0) {
+            uri = Uri.parse(videoTracks.get(videoTracks.size() - 1).getUrl());
+        } else if (videoQuality.equals("MEDIUM") && videoTracks.size() - 2 > 0) {
+            uri = Uri.parse(videoTracks.get(videoTracks.size() - 2).getUrl());
+        } else if (videoQuality.equals("LOW")) {
+            uri = Uri.parse(videoTracks.get(0).getUrl());
         } else {
-            uri = Uri.parse(mediaList.get(mediaList.size() - 1));
+            uri = Uri.parse(videoTracks.get(0).getUrl());
         }
         simpleExoPlayer.setPlayWhenReady(false);
         long position = simpleExoPlayer.getContentPosition();
@@ -204,23 +268,22 @@ public class MainActivity extends AppCompatActivity {
         ApiInterface apiInterface = retrofit.create(ApiInterface.class);
 
         Call<VideoInfo> videoInfoCall = apiInterface.getVideoInfo(id);
-        
+
         videoInfoCall.enqueue(new Callback<VideoInfo>() {
             @Override
             public void onResponse(Call<VideoInfo> call, Response<VideoInfo> response) {
-                if(response.isSuccessful()) {
+                if (response.isSuccessful()) {
                     try {
                         Files files = response.body().getRequest().getFiles();
-                        if(files.getProgressive() != null) {
+                        if (files.getProgressive() != null) {
                             List<Progressive> progressiveList = files.getProgressive();
-                            List<String> url = new ArrayList<>();
-                            for(int i=0; i<progressiveList.size(); i++) {
-                                url.add(progressiveList.get(i).getUrl());
+                            List<VideoTrack> videoTracks = new ArrayList<>();
+                            for (int i = 0; i < progressiveList.size(); i++) {
+                                videoTracks.add(new VideoTrack(progressiveList.get(i).getUrl(), progressiveList.get(i).getQuality()));
                             }
-                            if(url.size() > 0) {
-                                setMediaPlayback(url);
-                            }
-                            else
+                            if (videoTracks.size() > 0) {
+                                setMediaPlayback(videoTracks);
+                            } else
                                 Toast.makeText(MainActivity.this, "Error: Unable to fetch video!", Toast.LENGTH_SHORT).show();
                         }
                     } catch (Exception e) {
@@ -231,41 +294,85 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<VideoInfo> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "Oops! Something went wrong." + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Oops! Something went wrong." + t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
             }
         });
     }
 
-
-    private void setMediaPlayback(List<String> url) {
-        mediaList = url;
-        setMediaQuality("NORMAL");
+    private void setFullScreen() {
+        playerContainer.setLayoutParams(new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.MATCH_PARENT
+        ));
+        fullScreen.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, R.drawable.fullscreen_exit));
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        fullscreen = true;
     }
 
-    public void showMenu(View v)
-    {
-        PopupMenu popup = new PopupMenu(this, v);
+    private void exitFullScreen() {
+        float dpCalculation = getResources().getDisplayMetrics().density;
+        playerContainer.setLayoutParams(new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                (int) (250 * dpCalculation)
+        ));
+        fullScreen.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, R.drawable.fullscreen_open));
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        fullscreen = false;
+    }
 
-        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.video_quality_high:
-                        setMediaQuality("HIGH");
-                        break;
-                    case R.id.video_quality_medium:
-                        setMediaQuality("NORMAL");
-                        break;
-                    case R.id.video_quality_low:
-                        setMediaQuality("LOW");
-                        break;
-                }
-                return false;
-            }
-        });
-        MenuInflater inflater = popup.getMenuInflater();
-        inflater.inflate(R.menu.video_settings, popup.getMenu());
+    private void setMediaPlayback(List<VideoTrack> videoTracks) {
+        this.videoTracks = videoTracks;
+        setMediaQuality("MEDIUM");
+    }
+
+    public void showMenu(View v) {
         popup.show();
     }
 
+    @Override
+    public void onBackPressed() {
+        if (!fullscreen) super.onBackPressed();
+        else exitFullScreen();
+    }
+
+    private class Song {
+        private String title, artist, vimeoId;
+
+        public Song(String title, String artist, String vimeoId) {
+            this.title = title;
+            this.artist = artist;
+            this.vimeoId = vimeoId;
+        }
+
+        public String getArtist() {
+            return artist;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getVimeoId() {
+            return vimeoId;
+        }
+    }
+
+    private class VideoTrack {
+        private String url, quality;
+
+        public VideoTrack(String url, String quality) {
+            this.url = url;
+            this.quality = quality;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public String getQuality() {
+            return quality;
+        }
+    }
 }
